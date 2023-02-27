cd82d404c24fb5d01c65b8063511660555a21f12
Chigozie Oziri <blessedchigozie22@gmail.com>
2023-02-27 12:24:41 +0100

    calculator1.0.1 commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-A functioning Calculator App created with HTML, CSS and JavaScript.
+# A functioning Calculator App created with HTML, CSS and JavaScript.
+
+## Acknowledgement
+I acknowledge the following sources for the tutorials by which this project was made successful.
+- SkillUp by SimpliLearn
+- w3schools
+- AltSchool Africa
+- Stackoverflow
--- calc.js
+++ calc.js
@@ -1,3 +1,20 @@
+function display(val) {
+    //Create a list of all operators of the calculator
+    let operators = [...document.querySelectorAll('.operator')].map(el => el.textContent);
+    
+    //Get the current value
+    let resultElement = document.querySelector('#result');
+    let currentResult = resultElement.value;
+    
+    //When the input is an operator and the last character of the result is an operator then overwrite it, otherwise append it.
+    if(operators.includes(val) && operators.includes(currentResult.charAt(currentResult.length - 1))) {
+      resultElement.value = currentResult.substring(0, currentResult.length - 1) + val;
+    }
+    else {
+      resultElement.value += val;
+    }
+  }
+  
 const calculatorFunction = document.getElementById('result')
 
     function display(val) {
@@ -17,27 +34,4 @@ const calculatorFunction = document.getElementById('result')
 function darkModeFunction() {
     var element = document.body;
     element.classList.toggle("darkMode");
-    }
-    
-    function bulbFunction() {
-    var bulbLight = document.getElementById('bulb');
-    
-    if (bulbLight.src.match('bulbon'))
-        bulbLight.src = './img/bulboffbg.png';
-    else
-        bulbLight.src = './img/bulbon.jpg';
-    }
-    
-function darkModeFunction() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-    }
-    
-    function bulbFunction() {
-    var bulbLight = document.getElementById('bulb');
-    
-    if (bulbLight.src.match('bulbon'))
-        bulbLight.src = './img/bulboffbg.png';
-    else
-        bulbLight.src = './img/bulbon.jpg';
     }
