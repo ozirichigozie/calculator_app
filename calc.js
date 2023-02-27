c66177ad69e5adee7e430fcee907f6f94ad92b87
Chigozie Oziri <blessedchigozie22@gmail.com>
2023-02-27 12:59:09 +0100

    calculator_app1.0.3 commit

--- calc.js
+++ calc.js
@@ -1,20 +1,3 @@
-function display(val) {
-    //Create a list of all operators of the calculator
-    let operators = [...document.querySelectorAll('.operator')].map(el => el.textContent);
-    
-    //Get the current value
-    let resultElement = document.querySelector('#result');
-    let currentResult = resultElement.value;
-    
-    //When the input is an operator and the last character of the result is an operator then overwrite it, otherwise append it.
-    if(operators.includes(val) && operators.includes(currentResult.charAt(currentResult.length - 1))) {
-      resultElement.value = currentResult.substring(0, currentResult.length - 1) + val;
-    }
-    else {
-      resultElement.value += val;
-    }
-  }
-  
 const calculatorFunction = document.getElementById('result')
 
     function display(val) {
